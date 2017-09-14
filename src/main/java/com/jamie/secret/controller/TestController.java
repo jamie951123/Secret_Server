16069fb88ba24c2461b5090c3964ca02e4d92e35
Administrator <Administrator@nb1584887>
2017-09-14 16:53:00 +0800

    Token(16%)

--- src/main/java/com/jamie/secret/controller/TestController.java
+++ src/main/java/com/jamie/secret/controller/TestController.java
@@ -1,23 +1,15 @@
 package com.jamie.secret.controller;
 
-import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.jamie.secret.jwt.security.AccountCredentials;
-import com.jamie.secret.jwt.security.JwtUtil;
-import com.jamie.secret.jwt.security.ValidCredentialsService;
 import com.jamie.secret.model.TestModel;
 import com.jamie.secret.service.TestService;
 
@@ -25,9 +17,6 @@ import com.jamie.secret.service.TestService;
 @Controller
 public class TestController {
 	Logger log = LoggerFactory.getLogger(TestController.class);
-	
-	@Autowired
-	ValidCredentialsService validCredentialsService;
 	
 	@Autowired
 	private TestService testService;
